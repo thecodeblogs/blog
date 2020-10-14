e8dcca40b6d70cc499ea46ffe12d19d2abf2ccda
Admin <admin@thecodeblogs.com>
2020-10-14 07:20:28 -0400

    Initial commit

---
+++ projects/widgets/src/lib/widgets.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { WidgetsComponent } from './widgets.component';
+
+
+
+@NgModule({
+  declarations: [WidgetsComponent],
+  imports: [
+  ],
+  exports: [WidgetsComponent]
+})
+export class WidgetsModule { }
